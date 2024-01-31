a1f0b7b9a48e81114d78e958a4726dd99aa24cbf
diya4545 <diyakanwar0411d2h@gmail.com>
2024-01-31 16:52:47 +0530

    build for deployment

---
+++ dist/demo-blog/server/chunk-G2LKLGC2.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{Fb as s,K as i,Q as r,cc as m}from"./chunk-O5SA6UTX.mjs";import"./chunk-VVCT4QZE.mjs";var p=[{path:"",loadChildren:()=>import("./chunk-X3CH4J6O.mjs").then(o=>o.BlogModule)}],n=(()=>{let t=class t{};t.\u0275fac=function(e){return new(e||t)},t.\u0275mod=r({type:t}),t.\u0275inj=i({imports:[m.forChild(p),m]});let o=t;return o})();var l=(()=>{let t=class t{};t.\u0275fac=function(e){return new(e||t)},t.\u0275mod=r({type:t}),t.\u0275inj=i({imports:[s,n]});let o=t;return o})();export{l as UniversalModule};
---
+++ dist/demo-blog/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-2WD2FR7B.mjs";import"./chunk-NYQXGK53.mjs";import"./chunk-J2OJ6DRU.mjs";import"./chunk-O5SA6UTX.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
